64611c30644811aeaec60aeec5f7d084da42c07a
CyberSpartansFTC <sdstrobots@gmail.com>
2025-10-28 14:54:09 -0400

    Fix Gradle ZIP header error (will probably cause error again soon)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Test.java
@@ -0,0 +1,19 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+public class Test extends OpMode {
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void loop() {
+
+    }
+
+}
